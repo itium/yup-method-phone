c44d437b9cebc5b61e46689e9b76cd9b8817f468
Rodrigo Palomares <rodrigo.palomares@itium.mx>
2023-01-02 20:57:34 -0600

    Almost done. Release v0.9

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,5 @@
+// noinspection JSUnusedGlobalSymbols
+
 import pluginTerser from '@rollup/plugin-terser';
 import pluginTypescript from "@rollup/plugin-typescript";
 import pluginCommonjs from "@rollup/plugin-commonjs";
